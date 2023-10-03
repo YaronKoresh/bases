c292d6380c90840b15756895206e1b6e8fc43717
Yaron Koresh <aharonkoresh1@gmail.com>
2023-10-03 08:40:18 +0300

    Delete SPLIT.js

--- SPLIT.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(inp, num) {
-	inp = inp.toString();
-	var out = [];
-	for (var i = 0; i < inp.length; i += num) {
-		out.push(inp.substring(i, i + num))
-	}
-	return out
-}
