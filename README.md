a83f4f9bb7073df36b833fae911ead8408754fcb
Yaron Koresh <aharonkoresh1@gmail.com>
2023-10-10 00:07:59 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,27 +1,35 @@
 
-# @yaronkoresh/bases v3.0.0
+# @yaronkoresh/bases v4.0.0
 
 ### Made with much screen time & care for the community, by: Yaron Koresh, Israel.
 
-## This project is licensed under MIT open-source license.
+### This project is licensed under MIT open-source license.
 
 ### Base encoding/decoding algorithm, with the ability for custom character sets of choice.
 
-* Encode/Decode with your custom not standard base.
+* Converts bases with your custom not standard bases.
 
-* Encode/Decode Integer and string types.
+* Converts from/to text/number.
 
-* Choose a custom padding char (do not choose a character from the base you have chosen).
+* Converts between bases, using safe math operations (so... no errors with high numbers).
+
+* Choose a custom padding char (do not choose a digit OR a character included inside the charset).
+
+### Some bases, are still unsupported, for String type encoding / decoding (Number conversion is fully supported).
 
 # Example
 
 ```
 
+// ------------------------------------
+
 // Import this package
-import { Bases } from "@yaronkoresh/bases" // or: const { Bases } = await import("@yaronkoresh/bases");
+import { Bases } from "@yaronkoresh/bases"; // or: const { Bases } = await import("@yaronkoresh/bases");
+
+// ------------------------------------
 
 // "Bases" parameters:
-// parameter1: <input> a string OR integer.
+// parameter1: <input> the input to be converted.
 // parameter2: <charset> a string of the charset, eg. "0123456789abcdef".
 // parameter3: <'EncodeString'|'DecodeString'|'EncodeNumber'|'DecodeNumber'> choose the given input & action.
 // optional4: [paddingChar] the padding char in the end of the result string, default is '='.
@@ -32,22 +40,29 @@ import { Bases } from "@yaronkoresh/bases" // or: const { Bases } = await import
 // 'EncodeNumber' could be: 11.
 // 'DecodeNumber' could be: 10.
 
-const stringExample = Bases(
+// ------------------------------------
+
+const stringToBase64 = Bases(
 	"My name is Yaron Koresh, but everyone here calls me Yaroni Makaroni.",
-	"Gq8!(2-ABCDEFabcdef13456790",
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
 	1
 );
-console.log( stringExample );
-// The results: (aAC8G-b-q-a-28G-CA!8G2C-qA8-c-b8G(E-cA8-2A!-B8F8G-8A2A(8G-2A--2A8AC-c-b-28G-B-2A8-28G-!-q-F-FA!8G-a-28G2C-qA8-c-b-C8G(a-q-E-qA8-c-b-C8b
+console.log( stringToBase64 );
+// The results: 'TXkgbmFtZSBpcyBZYXJvbiBLb3Jlc2gsIGJ1dCBldmVyeW9uZSBoZXJlIGNhbGxzIG1lIFlhcm9uaSBNYWthcm9uaS4='
 
-const integerExample = Bases(
-	"255",
+// ------------------------------------
+
+const hexToDecimal = Bases(
+	"FF",
 	"0123456789ABCDEF",
-	'encodenumber'
+	10,
+	""
 );
-console.log( integerExample );
-// The results: FF
+console.log( hexToDecimal );
+// The results: 255
+
+// ------------------------------------
+
+// ENJOY!
 
 ```
-
-### Enjoy!
--- index.mjs
+++ index.mjs
@@ -1,37 +1,3 @@
-// ../_tools_/src/ConvertBytes.js
-var Decode = function(text) {
-  let result = [];
-  let i = 0;
-  text = encodeURI(text ?? "");
-  while (i < text.length) {
-    let c = text.charCodeAt(i++);
-    if (c == 37) {
-      result.push(parseInt(text.substr(i, 2), 16));
-      i += 2;
-    } else {
-      result.push(c);
-    }
-  }
-  return result;
-};
-var Encode = function(bytes) {
-  let result = [], i = 0;
-  while (i < bytes.length) {
-    let c = bytes[i];
-    if (c < 128) {
-      result.push(String.fromCharCode(c));
-      i++;
-    } else if (c > 191 && c < 224) {
-      result.push(String.fromCharCode((c & 31) << 6 | bytes[i + 1] & 63));
-      i += 2;
-    } else {
-      result.push(String.fromCharCode((c & 15) << 12 | (bytes[i + 1] & 63) << 6 | bytes[i + 2] & 63));
-      i += 3;
-    }
-  }
-  return result.join("");
-};
-
 // ../_tools_/src/Zeros.js
 var Zeros = function(str, len) {
   str = str.toString();
@@ -51,132 +17,257 @@ var Split = function(inp, num) {
   return out;
 };
 
-// ../_tools_/src/Bases.js
-var Bases = function(inp, charset, mode, paddingChar = "=") {
-  if (mode == 1) {
-    mode = "EncodeString";
-  } else if (mode == 0) {
-    mode = "DecodeString";
-  } else if (mode == 11) {
-    mode = "EncodeNumber";
-  } else if (mode == 10) {
-    mode = "DecodeNumber";
+// ../_tools_/src/Math.js
+var Greater = function(a, b) {
+  a = a.toString().split("");
+  b = b.toString().split("");
+  if (a.length > b.length) {
+    return a.join("");
   }
-  if (typeof inp == "string") {
-    inp = inp.split("");
-  } else if (typeof inp == "number") {
-    inp = [inp];
+  if (a.length < b.length) {
+    return b.join("");
   }
-  for (var i = 0; i < inp.length; i++) {
-    if (typeof inp[i] == "number") {
-      inp[i] = String.fromCharCode(inp[i]);
+  for (let i = 0; i < a.length; i++) {
+    if (+a[i] > +b[i]) {
+      return a.join("");
+    }
+    if (+a[i] < +b[i]) {
+      return b.join("");
     }
   }
-  inp = inp.join("");
-  if (typeof charset != "object") {
-    charset = charset.toString().split("");
-  }
-  var out = [];
-  var bs = charset.length;
-  if (bs < 2) {
-    return "";
-  }
-  var bits = Math.floor(Math.log2(bs));
-  var size = null;
-  var multi = 8;
-  while (multi % bits != 0) {
-    multi += 8;
-    size = multi / bits;
-  }
-  if (mode.toLowerCase() == "encodenumber") {
-    let UpdateOutputValue = function() {
-      let sum = 0;
-      for (let i2 = 0; i2 < outputCharsetIndexes.length; i2++) {
-        sum += Math.pow(bs, i2) * outputCharsetIndexes[i2];
-      }
-      outputValue = sum;
-    }, Power = function() {
-      return Math.pow(bs, outputIndexOutOfUsage);
-    };
-    let value = inp;
-    let outputCharsetIndexes = [];
-    let outputIndexOutOfUsage = 0;
-    let power = Power();
-    let outputValue = 0;
-    while (power <= value) {
-      outputCharsetIndexes.push(0);
-      outputIndexOutOfUsage++;
-      power = Power();
+  return true;
+};
+var Multiply = function(...nums) {
+  let ret = nums[0].toString();
+  nums = nums.slice(1);
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i].toString();
+    if (Add(num, 0) === "0") {
+      return "0";
     }
-    let currentOutputIndex = outputIndexOutOfUsage - 1;
-    while (currentOutputIndex >= 0) {
-      let indexValue = Math.pow(bs, currentOutputIndex);
-      while (outputValue + indexValue <= value) {
-        outputCharsetIndexes[currentOutputIndex]++;
-        outputValue += indexValue;
-      }
-      currentOutputIndex--;
-    }
-    out = outputCharsetIndexes.map(function(charsetIndex) {
-      return charset[charsetIndex];
-    }).reverse().join("");
-    return out;
-  } else if (mode.toLowerCase() == "decodenumber") {
-    let value = 0;
-    let outputCharsetIndexes = inp.split("").reverse().map(function(char2, index) {
-      value += charset.indexOf(char2) * Math.pow(bs, index);
-    });
-    return value;
-  } else if (mode.toLowerCase() == "encodestring") {
-    var result = "";
-    var ascii = Decode(inp);
-    var ascii_bits = "";
-    for (var j = 0; j < ascii.length; j++) {
-      ascii_bits += Zeros(ascii[j].toString(2), 8);
-    }
-    if (ascii_bits.length % bits != 0) {
-      ascii_bits += "0".repeat(bits - ascii_bits.length % bits);
-    }
-    var ordered = Split(ascii_bits, bits);
-    for (var j = 0; j < ordered.length; j++) {
-      result += charset[parseInt(ordered[j], 2)];
-    }
-    if (size != null) {
-      result += paddingChar.repeat((size - result.length % size) % size);
-    }
-    out = result;
-    return out;
-  } else if (mode.toLowerCase() == "decodestring") {
-    var chars = inp.split("");
-    var result = "";
-    var bin = "";
-    for (var j = 0; j < chars.length; j++) {
-      if (chars[j] == paddingChar) {
+    let value = ret;
+    let counter = "2";
+    while (true) {
+      if (Greater(counter, num) === counter) {
         break;
       }
-      var char = chars[j];
-      var dec = charset.indexOf(char);
-      if (dec == -1) {
-        return false;
-      }
-      bin += Zeros(dec.toString(2), bits);
+      ret = Add(ret, value);
+      counter = Add(counter, 1);
     }
-    let bin_array = Split(bin, 8);
-    try {
-      if (bin_array[bin_array.length - 1].length < 8) {
-        bin_array.pop();
-      }
-      for (var j = 0; j < bin_array.length; j++) {
-        bin_array[j] = parseInt(bin_array[j], 2);
-      }
-      result += Encode(bin_array);
-      out = result;
-    } catch (e) {
-      console.log("Error!");
-      return null;
-    }
-    return out;
   }
+  return ret;
+};
+var Power = function(...nums) {
+  let ret = nums[0].toString();
+  nums = nums.slice(1);
+  for (let i = 0; i < nums.length; i++) {
+    let num = nums[i].toString();
+    if (Add(num, 0) === "0") {
+      ret = "1";
+    }
+    let value = ret;
+    let counter = "2";
+    while (true) {
+      if (Greater(counter, num) === counter) {
+        break;
+      }
+      ret = Multiply(ret, value);
+      counter = Add(counter, 1);
+    }
+  }
+  return ret;
+};
+var Add = function(...nums) {
+  nums = nums.map((num) => num.toString().split("").reverse());
+  let ret = ["0"];
+  for (let i = 0; i < Math.max(...nums.map((num) => num.length)); i++) {
+    let sum = 0;
+    nums.map((num) => parseInt(num[i] ?? 0)).map(function(n) {
+      sum += n;
+    });
+    sum = sum.toString().split("").reverse();
+    for (let j = 0; j < sum.length; j++) {
+      let index = j + i;
+      ret[index] = ret[index] ?? "0";
+      let n = (parseInt(ret[index]) + parseInt(sum[j] ?? 0)).toString().split("").reverse();
+      ret[index] = n[0];
+      if (n.length > 1) {
+        index++;
+        n = n.slice(1).join("");
+        let retToBeChanged = ret.slice(index).reverse().join("");
+        ret = ret.slice(0, index + 1);
+        ret.push(...Add(retToBeChanged, n).split("").reverse());
+      }
+    }
+  }
+  return ret.reverse().join("");
+};
+
+// ../_tools_/src/Round.js
+var RoundUp = function(input, factor) {
+  return factor - input % factor + input;
+};
+
+// ../_tools_/src/Bits.js
+var MeasureBits = function(num, roundUp = true) {
+  return roundUp == true ? Math.ceil(Math.log2(num)) : Math.log2(num);
+};
+
+// ../_tools_/src/Unicode.js
+var NumberToCharset = function(charset, num) {
+  charset = charset.split("");
+  let bs = charset.length;
+  const UpdateOutputValue = function(outputCharsetIndexes2) {
+    let sum = "0";
+    for (let i2 = 0; i2 < outputCharsetIndexes2.length; i2++) {
+      sum = Add(sum, Multiply(Power(bs, i2), outputCharsetIndexes2[i2]));
+    }
+    return sum;
+  };
+  let value = num.toString();
+  let outputCharsetIndexes = ["0"];
+  let outputIndexOutOfUsage = 1;
+  let power = Power(bs, outputIndexOutOfUsage);
+  let outputValue = "0";
+  while (Greater(power, value) !== power) {
+    outputCharsetIndexes.push("0");
+    outputIndexOutOfUsage++;
+    power = Power(bs, outputIndexOutOfUsage);
+  }
+  let currentOutputIndex = outputIndexOutOfUsage - 1;
+  while (currentOutputIndex >= 0) {
+    let indexValue = Power(bs, currentOutputIndex);
+    outputValue = UpdateOutputValue(outputCharsetIndexes);
+    let _outputValue = Add(indexValue, outputValue);
+    while (Greater(_outputValue, value) !== _outputValue) {
+      outputCharsetIndexes[currentOutputIndex] = Add(outputCharsetIndexes[currentOutputIndex], 1);
+      outputValue = _outputValue;
+      _outputValue = Add(indexValue, outputValue);
+    }
+    currentOutputIndex--;
+  }
+  return outputCharsetIndexes.map(function(charsetIndex) {
+    return charset[+charsetIndex];
+  }).reverse().join("");
+};
+var CharsetToNumbers = function(str, charset = null) {
+  let ret = [];
+  str = str.split("").reverse().join("");
+  for (let i = 0; true; i++) {
+    if (typeof str.codePointAt(i) === "undefined") {
+      ret = ret.reverse();
+      while (true) {
+        if (ret.length > 0 && ret[0] === charset.split("")[0]) {
+          ret = ret.slice(1);
+        } else {
+          return ret;
+        }
+      }
+    }
+    ret.push(
+      charset === null ? str.codePointAt(i).toString() : Multiply(Power(charset.length, i), charset.indexOf(str.split("")[i]))
+    );
+  }
+};
+var NumbersToCharset = function(nums, charset = null) {
+  nums = [nums].flat();
+  if (charset === null) {
+    return String.fromCodePoint(...nums.map((value) => parseInt(value))).toString();
+  }
+  let bits = MeasureBits(charset.length);
+  let size = RoundUp(bits, 8) / bits;
+  let ret = nums.map((value) => NumberToCharset(charset, value)).join("");
+  while (true) {
+    if (ret.length > 1 && ret.slice(0, 1) === charset.split("")[0]) {
+      ret = ret.slice(1);
+    } else {
+      return ret;
+    }
+  }
+};
+var StringToBytes = function(str) {
+  return [...new TextEncoder().encode(str)];
+};
+var BytesToString = function(...bytes) {
+  bytes = [bytes].flat().flat();
+  bytes = new Uint8Array(bytes);
+  return new TextDecoder().decode(bytes);
+};
+
+// ../_tools_/src/Bases.js
+var Bases = function(str, charset, mode, padding = "=") {
+  let from = null;
+  let to = null;
+  if (mode.toString() === "1" || mode.toString().toLowerCase() === "encodestring") {
+    to = charset;
+  } else if (mode.toString() === "0" || mode.toString().toLowerCase() === "decodestring") {
+    from = charset;
+  } else if (mode.toString() === "11" || mode.toString().toLowerCase() === "encodenumber") {
+    from = "0123456789";
+    to = charset;
+  } else if (mode.toString() === "10" || mode.toString().toLowerCase() === "decodenumber") {
+    to = "0123456789";
+    from = charset;
+  }
+  str = str.toString();
+  from = from === null ? from : from.toString();
+  to = to === null ? to : to.toString();
+  let toLength = to === null ? null : to.length;
+  if (typeof toLength === "number" && toLength < 2) {
+    return null;
+  }
+  let toBits = toLength === null ? null : MeasureBits(toLength);
+  let toFloatBits = toLength === null ? null : MeasureBits(toLength, false);
+  let toSize = toBits === null ? null : RoundUp(toBits, 8) / toBits;
+  let toFloat = toFloatBits !== parseInt(toFloatBits);
+  let fromLength = from === null ? null : from.length;
+  if (typeof fromLength === "number" && fromLength < 2) {
+    return null;
+  }
+  let fromBits = fromLength === null ? null : MeasureBits(fromLength);
+  let fromFloatBits = fromLength === null ? null : MeasureBits(fromLength, false);
+  let fromSize = fromBits === null ? null : RoundUp(fromBits, 8) / fromBits;
+  let fromFloat = fromFloatBits !== parseInt(fromFloatBits);
+  let num = "";
+  let out = [];
+  if (from === to) {
+    return str;
+  }
+  if (from !== null) {
+    let re = new RegExp("(" + padding + "){1,}$", "g");
+    str = str.replaceAll(re, "");
+  }
+  if (from !== null && to !== null) {
+    CharsetToNumbers(str, from).reverse().map(function(value) {
+      num = Add(num, value);
+    });
+    out = NumbersToCharset(num, to);
+  } else if (from === null && to !== null && toFloat === true) {
+    console.error("String Encoding/Decoding with some bases may be unsupported!");
+    console.error("Base" + toLength + " is unfortunately one of them.");
+    return false;
+  } else if (from === null && to !== null) {
+    let bytes = StringToBytes(str);
+    let bin = bytes.map((byte) => Zeros((+byte).toString(2), 8)).join("");
+    bin += "0".repeat((toBits - bin.length % toBits) % toBits);
+    let bins = Split(bin, toBits);
+    let values = bins.map((b) => parseInt(b, 2));
+    out = NumbersToCharset(values, to);
+  } else if (from !== null && to === null && fromFloat === true) {
+    console.error("String Encoding/Decoding with some bases may be unsupported!");
+    console.error("Base" + toLength + " is unfortunately one of them.");
+    return false;
+  } else if (from !== null && to === null) {
+    let charsLength = Split(str, fromSize).length;
+    let values = CharsetToNumbers(str, from);
+    let bin = values.map((v) => Zeros(parseInt(v).toString(2), fromBits)).join("").slice(0, charsLength * 8);
+    let bytes = Split(bin, 8).map((b) => parseInt(b, 2));
+    out = BytesToString(bytes);
+  }
+  if (toSize !== null) {
+    out += padding.repeat((toSize - out.length % toSize) % toSize);
+  }
+  return out;
 };
 export {
   Bases
